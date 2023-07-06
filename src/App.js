ef7a28d18145d1a0f47b64740c1ecf44fbf956e0
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-06 17:20:53 +0200

    Fin de la mise en place du premier composant

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Component 1
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sympa <code>le petit</code> JSX.
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          C'est parti !
         </a>
       </header>
     </div>
