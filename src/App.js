116624c911decf98075b0a871266d7bece591c4a
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-06 18:43:32 +0200

    Classes & Functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Mycars from './components/Mycars';
+import Car from './components/Cars';
 
 // Component 1
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
           <Mycars />
+          <Car />
       </div>
     );
   }
---
+++ src/components/Cars.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Car = () => {
+    return (
+        <div>
+            <p>Marque :</p>
+            <p>Couleur :</p>
+        </div>
+        
+    )
+}
+
+export default Car;
