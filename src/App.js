355fd6c1dfb4cc13a30572e344001e34c1882d40
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-06 18:31:32 +0200

    Imbrication de deux composants

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Mycars from './components/Mycars';
 
 // Component 1
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sympa <code>le petit</code> JSX.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          C'est parti !
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+          <Mycars />
+      </div>
+    );
+  }
+
 }
 
 export default App;
