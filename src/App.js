5a32869651a371d2e5a7abd28d529a234b2d7180
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-06 19:40:03 +0200

    States and classes relations

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import Mycars from './components/Mycars';
 // Component 1
 class App extends Component {
 
+  state = {
+    title: 'Voiture gratuite'
+  }
+
   render() {
     return (
       <div className="App">
-          <Mycars />
+          { /* J'envoie mon props dans mon composant, il sera alors accessible en tant que props */}
+          <Mycars title={this.state.title}/>
       </div>
     );
   }
--- src/components/Mycars.jsx
+++ src/components/Mycars.jsx
@@ -5,7 +5,7 @@ class Mycars extends Component {
     render() {
         return (
             <div>
-                <h1>Catalogue voiture</h1>
+                <h1>{this.props.title}</h1>
                 <Car color="red">Ford</Car>
                 <Car>Mercedes</Car>
                 <Car color="green"></Car>               
