7c26b9603a8b2ba2c6f5497f118fa7c75ad0c001
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-21 19:09:17 +0200

    Revision

--- src/App.js
+++ src/App.js
@@ -1,55 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import { Component } from 'react';
 import Mycars from './components/Mycars';
+import './App.css';
 
-
-// Component 1
 class App extends Component {
 
   state = {
-    title: 'Mon catalogue voiture 2',
-    color: 'green'
+    titre: 'Mon Catalogue Voitures'
   }
-
-  changeTitle = (e) => {
-      this.setState({
-        title : 'Mon nouveau titre'
-      })    
-  }
-
-  changeViaParam = (newTitle) => {
-    // Using setState allow react to automatically detect any change and reload the render() method
-    this.setState({
-      title : newTitle
-    })
-  }
-
-  changeViaBind = (param) => {
-    this.setState({
-      title : param
-    })
-  }
-
-  changeViaInput = (e) => {
-    this.setState({
-      title : e.target.value
-    })
-  }
-
 
   render() {
     return (
-      <div className="App">
-          { /* J'envoie mon props dans mon composant, il sera alors accessible en tant que props */}
-          <Mycars title={this.state.title} color={this.state.color}/>
-          <button onClick={this.changeTitle}>Changer le nom en dur</button>
-          <button onClick={() => this.changeViaParam('Parameter')}>Parameter</button>
-          <button onClick={this.changeViaBind.bind(this, 'Titre via bind')}>Via bind</button>
-          <input type="text"onChange={this.changeViaInput} value={this.state.title}/>
+      <div className='App'>
+        <Mycars title={this.state.titre} />
       </div>
-    );
+    )
   }
-
 }
 
 export default App;
--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,38 +1,17 @@
-// import React from 'react';
-// import Wrapper from './Wrapper'
-// // With destructuring, we can access data inside an object without the need of a variable name
-// const Car = ({children, color}) => {
+const Car = ({color, children}) => {
 
-//     let colorInfo = "";
+    const colorInfo = color ? <p>Couleur: {color}</p> : <p>Couleur: "Néant"</p>;
 
-//     if (color) {
-//         colorInfo = color;
-//     } else {
-//         colorInfo = "Néant";
-//     }
+    if (children) {
+        return (
+            <div style={ {backgroundColor: 'pink', width: '400px', padding: '10px', margin: '5px auto'} }>
+                <p>Marque: { children }</p>
+                {colorInfo}
+            </div>
+        )
+    }
 
-//     if (children) {
-//         return (
-//             <Wrapper>
-//                 <p>Marque: { children }</p>
-//                 {/* { color ? <p>Couleur : {color} </p> : <p>Couleur : néant </p>} */}
-//                 {/* <p>{color ? color : "Néant" }</p> */}
-//                 <p>Couleur : {colorInfo}</p>  
-//             </Wrapper> 
-//         )
-//     } else {
-//         return (
-//             <Wrapper>
-//                 <p>Non renseigné</p>   
-//             </Wrapper>
-//         )
-//     }
+    // return null // ce return est Facultatif
+}
 
-
-
-//     // On peut également retourner de cette façon : 
-//     // return children ? (mon jsx) : (mon jsx indiquant qu'il n'y a aucune information)
-    
-// }
-
-// export default Car;
+export default Car
